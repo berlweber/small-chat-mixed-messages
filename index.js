4b914c93aef13d7f5968407d98167c2e403652ec
Berl Weber <153825363+berlweber@users.noreply.github.com>
2025-01-07 13:19:23 +0100

    added multiple choices for the conversation parts

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 1. An opening greeting. 2. An opening question. 3  An answer from ‘me’. 4. A closing greeting.
 every time it is run it uses randomly chosen something different for each part.*/
 
-const LsOfOpeningGreetings =[];
-const lsOfQuestions = [];
-const lsOfAnswers = [];
-const lsOfClosingGreetings = [];
+//arrays to store multiple choices for each of the conversation parts
+const LsOfOpeningGreetings =["Hi there, ", "Hello, ", "Dear Mr. or Mrs., ", "Good morning, ", "Good afternoon, ", "Good evening, ",];
+const lsOfQuestions = ["How are you? ", "How is it going? ", "How are you feeling today? ", "What's up?", "What's going on with you? Long time didn't see you!", "are things moving in a good direction?",];
+const lsOfAnswers = ["Fine. ", "Not much. ", "Things are getting better rapidly. ", "I am so happy today!!!! ", "It's the same as yesterday. ", "Yeah, you know…. Thats life.",];
+const lsOfClosingGreetings = ["Have a nice day. ", "Good evening. ", "Was great meeting you. ", "I enjoyed talking to you. ", "by by. ",];
+
