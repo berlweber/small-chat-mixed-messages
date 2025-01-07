d0a61e3da8de55664b8c1f0985a596a331ef70f9
Berl Weber <153825363+berlweber@users.noreply.github.com>
2025-01-07 12:53:20 +0100

    comment at top of file about the program goal and structure

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*the program will print a mock-up small chat, with 4 part put together;
+1. An opening greeting. 2. An opening question. 3  An answer from ‘me’. 4. A closing greeting.
+every time it is run it uses randomly chosen something different for each part.*/
+
 const LsOfOpeningGreetings =[];
 const lsOfQuestions = [];
 const lsOfAnswers = [];
